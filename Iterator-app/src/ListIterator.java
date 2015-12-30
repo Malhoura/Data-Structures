72d7dff424b46a433a6f51a6b8058ae71f9dd33a
Malhoura <malhoura@hawk.iit.edu>
2015-12-29 20:51:40 -0600

    Added methods
    iterator methods added (reset, nextLink, getCurrent, atEnd, insertAfter,
    insertBefoore)

--- Iterator-app/src/ListIterator.java
+++ Iterator-app/src/ListIterator.java
@@ -1,11 +1,64 @@
 
 public class ListIterator {
 	
+	private Link current;
 	private Link previous;
-	private Link next;
-	private LinkList theList;
+	private LinkList ourList;
 	
 	public ListIterator(LinkList list){
-		this.theList = list;
+		ourList = list;
+		reset();
 	}
+	
+	public void reset(){
+		current = ourList.getFirst();
+		previous = null;
+	}
+	
+	public void nextLink(){	
+		previous = current;
+		current = current.next;
+}
+	
+	public boolean atEnd(){
+		return (current.next == null);
+	}
+	
+	public Link getCurrent(){
+		return current;
+	}
+	
+	public void insertAfter(long d){
+		
+		Link newLink = new Link(d);
+		
+		if(ourList.isEmpty()){
+			ourList.setFirst(newLink);
+			current = newLink;
+		}
+		else{	
+		newLink.next = current.next;
+		current.next = newLink;
+		nextLink();
+		}
+	}
+	
+	public void insertBefore(long d){
+		Link newLink = new Link(d);
+		
+		if(previous == null){
+			newLink.next = ourList.getFirst();
+			ourList.setFirst(newLink);
+			reset();
+		}
+		else{
+			newLink.next = previous.next;
+			previous.next = newLink;
+			current = newLink;
+		}
+		
+	}
+	
+	
+	
 }
