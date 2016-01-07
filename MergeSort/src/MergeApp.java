2edf5f9539e878fa6cc35f2cb8746d058fd5de7e
Malhoura <malhoura@hawk.iit.edu>
2016-01-07 15:48:51 -0600

    Comments added
    A description of how merge works in case of a two already sorted arrays

--- MergeSort/src/MergeApp.java
+++ MergeSort/src/MergeApp.java
@@ -11,29 +11,41 @@ public class MergeApp {
 
 	}
 	
+	//merge function has five arguments passed to it
 	public static void merge(int[] arrayA, int sizeA, int [] arrayB, int sizeB, int [] arrayC){
 		
 		int i=0;
 		int j=0;
 		int k=0;
 		
+		//when both arrays A and B are not empty
 		while(i<sizeA && j<sizeB){
+			//if value at arrayA is smaller that the value at arrayB
 			if(arrayA[i] < arrayB[j]){
+				//merge this value to the new array (arrayC)
 				arrayC[k] = arrayA[i];
 			i++;
 			}else{
+				//otherwise merge the value at arrayB to the new arrayC
 				arrayC[k] = arrayB[j];
 				
 				j++;
 			}
+			//increment arrayC index when finished merging
 			k++;
 		}
+		//when the two arrays are not equal in length
+		//and arrayA larger than arrayB
+		//merge the rest of the sorted elements of arrayA to the new arrayC and increment both indexes
 		while(i < sizeA){
 			arrayC[k] = arrayA[i];
 			i++;
 			k++;
 		}
-			
+		
+		//when the two arrays are not equal in length
+		//and arrayB larger than arrayA
+		//merge the rest of the sorted elements of arrayB to the new arrayC and increment both indexes
 		while(j < sizeB){
 			arrayC[k] = arrayB[j];
 			j++;
@@ -41,6 +53,7 @@ public class MergeApp {
 		}
 	}
 	
+	// displaying sorted array's elements
 	public static void display(int [] theArray, int num){
 		
 		for(int i=0; i < num; i++){
